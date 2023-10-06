688c37f75dc022158c592549104329561ec4aa6a
samuel21695 <samuel21695@gmail.com>
2023-10-06 11:26:54 +0900

    write function sosa that works like setInterval()

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 setTimeout(function() {
   console.log('첫번째 소사');
- 
-  setTimeout(function() {
-    console.log('두번째 문기');
-
-    setTimeout(function() {
-      console.log('세번째 민구');
-    }, 1000);
-
-  }, 2000);
-  
 }, 3000
 );
+
+setTimeout(function() {
+  console.log('두번째 문기');
+}, 2000);
+
+setTimeout(function() {
+  console.log('세번째 민구');
+}, 1000);
+
+const sosa = setTimeout(function() {
+  sosa();
+}, 1000);
+
+function sosa() {
+  setTimeout(function() {
+    sosa();
+  }, 1000);
+};
