cd9d333a4601d68a7d0e20a9d549f59afb05b03c
samuel21695 <samuel21695@gmail.com>
2023-10-06 10:47:45 +0900

    put second function into the first function to be executed 5sec later

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 setTimeout(function() {
   console.log('첫번째 소사');
-}, 3000);
-setTimeout(function() {
-  console.log('두번째 문기');
-}, 2000);
+  setTimeout(function() {
+    console.log('두번째 문기');
+  }, 2000);
+}, 3000
+);
+
 setTimeout(function() {
   console.log('세번째 민구');
 }, 1000);
