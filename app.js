20358873d263a401cf265a7707bda5073e86b228
samuel21695 <samuel21695@gmail.com>
2023-10-06 10:48:59 +0900

    put third function into the second function to be executed 6sec later

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ setTimeout(function() {
   console.log('첫번째 소사');
   setTimeout(function() {
     console.log('두번째 문기');
+    setTimeout(function() {
+      console.log('세번째 민구');
+    }, 1000);
   }, 2000);
 }, 3000
 );
-
-setTimeout(function() {
-  console.log('세번째 민구');
-}, 1000);
